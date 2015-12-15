e3c3cf4b55000ddcd22d31d4fb223be1c1f95547
onigra <3280467rec@gmail.com>
2015-12-15 20:24:27 +0900

    This is it.

---
+++ lib/planaria.rb
@@ -0,0 +1,9 @@
+require "planaria/version"
+require "thor"
+require "yaml"
+require "fileutils"
+require "erb"
+
+require "planaria/cli"
+require "planaria/generator/initializer"
+require "planaria/generator/builder"
---
+++ lib/planaria/version.rb
@@ -0,0 +1,3 @@
+module Planaria
+  VERSION = "0.1.0"
+end

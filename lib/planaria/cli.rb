f3d1d2fcc64e9ecc3a91143b2c70bc9b1d29321f
onigra <3280467rec@gmail.com>
2015-12-19 23:19:00 +0900

    remove .

--- lib/planaria/cli.rb
+++ lib/planaria/cli.rb
@@ -1,19 +1,19 @@
 module Planaria
   class CLI < Thor
 
-    desc "init", "Create new template directories."
+    desc "init", "Create new template directories"
     option :name, type: :string, aliases: '-n', required: true
     def init
       Generator::Initializer::Runner.new(options[:name]).run
     end
 
-    desc "build", "Create template."
+    desc "build", "Create template"
     option :name, type: :string, aliases: '-n', required: true
     def build
       Generator::Builder::Runner.new(options[:name]).run
     end
 
-    desc "devide", "Devide project."
+    desc "devide", "Devide project"
     option :name, type: :string, aliases: '-n', required: true
     option :base, type: :string, aliases: '-b', required: true
     def devide

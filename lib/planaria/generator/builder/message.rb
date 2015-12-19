cf48e613c1896a7c60af9360e0ed8ff38e897394
onigra <3280467rec@gmail.com>
2015-12-19 19:08:09 +0900

    redesign builder

---
+++ lib/planaria/generator/builder/message.rb
@@ -0,0 +1,12 @@
+module Planaria::Generator
+  module Builder
+    class Message
+      def initialize(name)
+        @name = name
+      end
+
+      def run
+      end
+    end
+  end
+end
---
+++ lib/planaria/generator/builder/runner.rb
@@ -0,0 +1,23 @@
+module Planaria::Generator
+  module Builder
+    class Runner
+      def initialize(name)
+        @name = name
+      end
+
+      def run
+        execute
+      end
+
+      private
+
+      def execute
+        Executer.new(@name).run
+      end
+
+      def message
+        Message.new(@name).run
+      end
+    end
+  end
+end
